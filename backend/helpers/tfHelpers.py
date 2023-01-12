a2ac388f988263f5e42c4c12708342a2af7291d0
andrew-up <aageew@bk.ru>
2023-01-12 12:36:48 +0300

    upd

--- backend/helpers/tfHelpers.py
+++ backend/helpers/tfHelpers.py
@@ -3,8 +3,12 @@ from tensorflow import config
 
 def setLimitGPU(memorySizeMB):
 
-    gpus = config.experimental.list_physical_devices('GPU')  # Получить список GPU
-    if gpus is not None:
-        config.experimental.set_virtual_device_configuration(gpus[0], [
-            config.experimental.VirtualDeviceConfiguration(memory_limit=memorySizeMB)])
+    try:
+        gpus = config.experimental.list_physical_devices('GPU')  # Получить список GPU
+        if gpus is not None:
+            print('Используется GPU')
+            config.experimental.set_virtual_device_configuration(gpus[0], [
+                config.experimental.VirtualDeviceConfiguration(memory_limit=memorySizeMB)])
+    except Exception:
+        print('Нет GPU')
     # Первый параметр - это принцип, какой GPU, только один - gpu [0], memory_limt позади - это ограниченный размер видеопамяти, единица - M
