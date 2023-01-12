9f477c84a0250996386c4f8c0562b5b5781c3243
andrew-up <aageew@bk.ru>
2023-01-12 12:38:22 +0300

    upd

--- backend/helpers/tfHelpers.py
+++ backend/helpers/tfHelpers.py
@@ -6,9 +6,9 @@ def setLimitGPU(memorySizeMB):
     try:
         gpus = config.experimental.list_physical_devices('GPU')  # Получить список GPU
         if gpus is not None:
-            print('Используется GPU')
             config.experimental.set_virtual_device_configuration(gpus[0], [
                 config.experimental.VirtualDeviceConfiguration(memory_limit=memorySizeMB)])
+            print('Используется GPU')
     except Exception:
         print('Нет GPU')
     # Первый параметр - это принцип, какой GPU, только один - gpu [0], memory_limt позади - это ограниченный размер видеопамяти, единица - M
